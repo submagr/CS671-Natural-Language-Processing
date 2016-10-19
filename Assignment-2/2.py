cdf4713ba9286e7c659caf9b100a4532063c3b67
submagr <agshubh191@gmail.com>
2016-10-19 21:17:49 +0530

    Used google vector

--- Assignment-2/2.py
+++ Assignment-2/2.py
@@ -3,16 +3,19 @@ from nltk.corpus import brown
 from sklearn.metrics.pairwise import cosine_similarity
 import matplotlib.pyplot as plt
 
-model = Word2Vec(brown.sents()[1:10])
+googleModel = Word2Vec.load_word2vec_format('models/GoogleNews-vectors-negative300.bin', binary=True)  
+print "Google model loaded"
 
-def getGoogleVec(word):
-    #TODO: Return real google vector here
-    return model[word]
+# Caps => Small
+sentences = [[word.lower() for word in sentence] for sentence in brown.sents()]
+model = Word2Vec(brown.sents(), size = 300)
+print "Skip gram trained on Brown words"
 
 cosineSim = []
+print "Computing Cosine Similarities"
 for word in model.vocab:
-    googleVec = getGoogleVec(word)
-    cosineSim.append(cosine_similarity(googleVec, model[word])[0][0])
+    if word in googleModel:
+        cosineSim.append(cosine_similarity(googleModel[word], model[word])[0][0])
 
-plt.hist(cosineSim, bins = [x*0.1 for x in range(0,11)])
+plt.hist(cosineSim, bins = [x*0.1 for x in range(-10,11)])
 plt.show()
